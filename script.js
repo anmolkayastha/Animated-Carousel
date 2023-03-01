8d079b41119929807c0e5764855e4945c0b5e637
Anmol Kayastha <74373013+anmolkayastha@users.noreply.github.com>
2023-02-28 22:29:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,20 @@ buttons.forEach(button => {
         console.log(slides);
         
         const activeSlide = slides.querySelector("[data-active]")
-        
+
+        // Convert slides to an array
+        // Get index of element
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
         console.log(activeSlide);
+
+        // Index of last element
+        if(newIndex < 0) newIndex = slides.children.length - 1
+        // Passed last slide
+        if(newIndex >= slides.children.length) newIndex = 0
+
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
     })
 })
 
